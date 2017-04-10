3a0df9256b4ff03eef4e62df2f1f0dae722889aa
Gian26 <giancuello26@gmail.com>
2017-04-09 18:13:15 -0600

    Changed to a grid setting, added a status bar, tool bar.

--- videoPlayerWPF/MainWindow.xaml.cs
+++ videoPlayerWPF/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace videoPlayerWPF
 {
@@ -21,22 +23,41 @@ namespace videoPlayerWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool mediaPlayerIsPlaying = false;
+        private bool userIsDraggingSlider = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
             // Initialize the MediaElement property values.
             InitializePropertyValues();
 
         }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if ((Player.Source != null) && (Player.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
+            {
+                sliProgress.Minimum = 0;
+                sliProgress.Maximum = Player.NaturalDuration.TimeSpan.TotalSeconds;
+                sliProgress.Value = Player.Position.TotalSeconds;
+            }
+        }
+
 
         private void Media_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.AddExtension = true;
-            ofd.DefaultExt = "*.*";
-            ofd.Filter = "Media(*.*)|*.*";
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                AddExtension = true,
+                DefaultExt = "*.*",
+                Filter = "Media(*.*)|*.*"
+            };
             ofd.ShowDialog();
-            Player.MediaOpened += new RoutedEventHandler(Player_MediaOpened);
+            //Player.MediaOpened += new RoutedEventHandler(Player_MediaOpened);
             Player.Source = new Uri(ofd.FileName);
         }
 
@@ -52,7 +73,7 @@ namespace videoPlayerWPF
 
         private void Player_MediaOpened(object sender, RoutedEventArgs e)
         {
-            MediaName.Content = Player.Source.ToString();
+           // MediaName.Content = Player.Source.ToString();
         }
 
         private void Play_Click(object sender, RoutedEventArgs e)
@@ -71,15 +92,20 @@ namespace videoPlayerWPF
             this.SizeChanged += Window_SizeChanged;
         }
 
-        // Jump to different parts of the media (seek to). 
-        private void SeekToMediaPosition(object sender, RoutedPropertyChangedEventArgs<double> args)
+        private void SliProgress_DragStarted(object sender, DragStartedEventArgs e)
         {
-            int SliderValue = (int)timelineSlider.Value;
+            userIsDraggingSlider = true;
+        }
 
-            // Overloaded constructor takes the arguments days, hours, minutes, seconds, miniseconds.
-            // Create a TimeSpan with miliseconds equal to the slider value.
-            TimeSpan ts = new TimeSpan(0, 0, 0, 0, SliderValue);
-            Player.Position = ts;
+        private void SliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            userIsDraggingSlider = false;
+            Player.Position = TimeSpan.FromSeconds(sliProgress.Value);
+        }
+
+        private void SliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblProgressStatus.Text = TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
         }
 
         // Change the volume of the media.
@@ -92,7 +118,7 @@ namespace videoPlayerWPF
         // to the total number of miliseconds in the length of the media clip.
         private void Element_MediaOpened(object sender, EventArgs e)
         {
-            timelineSlider.Maximum = Player.NaturalDuration.TimeSpan.TotalMilliseconds;
+            //timelineSlider.Maximum = Player.NaturalDuration.TimeSpan.TotalMilliseconds;
         }
 
         // When the media playback is finished. Stop() the media to seek to media start.
@@ -105,7 +131,7 @@ namespace videoPlayerWPF
         // Change the speed of the media.
         private void ChangeMediaSpeedRatio(object sender, RoutedPropertyChangedEventArgs<double> args)
         {
-            Player.SpeedRatio = (double)speedRatioSlider.Value;
+            //Player.SpeedRatio = (double)speedRatioSlider.Value;
         }
 
         void InitializePropertyValues()
@@ -113,7 +139,12 @@ namespace videoPlayerWPF
             // Set the media's starting Volume and SpeedRatio to the current value of the
             // their respective slider controls.
             Player.Volume = (double)volumeSlider.Value;
-            Player.SpeedRatio = (double)speedRatioSlider.Value;
+            //Player.SpeedRatio = (double)speedRatioSlider.Value;
         }
+        private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Player.Volume += (e.Delta > 0) ? 0.1 : -0.1;
+        }
+
     }
 }
